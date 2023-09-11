93d6f23ad91e621069bcd95fd415b45d9be05e3a
Re3eTa <144643095+Re3eTa@users.noreply.github.com>
2023-09-11 17:06:25 +1200

    Update app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+#sets up website
+st.set_page_config(title="Fynn's website", icon=":wave:", layout="wide")
+#contents
+st.subheader("Hi!, I am Fynn! :wave:")
+st.title("A fresh student majoring in the electronics and computer systems.")
+st.write("i am passionate about te way electricity works and how the average person can use forms of eletrical theory in the real world.")
+st.write("hopefully by the time I graduate ive put enough energy into this project that i can show you other wibsites that ive made, Maybe ill even start up a blog?")
